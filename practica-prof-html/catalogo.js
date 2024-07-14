32a3a5d8cad9423e93e3bcb8a3e631f2a9487da0
Gaspar <shedinja1999@gmail.com>
2024-07-14 14:45:33 -0300

    Implementacion de Modulos y JSON

--- practica-prof-html/catalogo.js
+++ practica-prof-html/catalogo.js
@@ -1,100 +1,29 @@
 //Importaciones
-//import make_innerHTML from "./funciones"
+import { make_innerHTML } from "./funciones.js"
 
 //Referencias al DOM
 const Tienda = document.getElementById('Tienda')
 console.log(Tienda)
 
 //Productos
-const tienda = {
-    categorias: [
-        [
-            {
-                nombre: "Latita",
-                rutaImagen: "./Recursos/latas.jpg",
-                precio: 2000,
-                categoria: "Cumpleaños"
-            },
-            {
-                nombre: "Latita",
-                rutaImagen: "./Recursos/latas.jpg",
-                precio: 2000,
-                categoria: "Cumpleaños"
-            },
-            {
-                nombre: "Latita",
-                rutaImagen: "./Recursos/latas.jpg",
-                precio: 2000,
-                categoria: "CUMPLEAÑOS"
-            }
-        ],
-        [
-            {
-                nombre: "Bolsa",
-                rutaImagen: "./Recursos/bolsas.jpg",
-                precio: 2000,
-                categoria: "Comunion"
-            },
-            {
-                nombre: "Bolsa",
-                rutaImagen: "./Recursos/bolsas.jpg",
-                precio: 2000,
-                categoria: "Comunion"
-            },
-            {
-                nombre: "Bolsa",
-                rutaImagen: "./Recursos/bolsas.jpg",
-                precio: 2000,
-                categoria: "Comunion"
-            }
-        ],
-        [
-            {
-                nombre: "Tarjetas",
-                rutaImagen: "./Recursos/tarjetas.jpg",
-                precio: 2000,
-                categoria: "Souvenir"
-            },
-            {
-                nombre: "Tarjetas",
-                rutaImagen: "./Recursos/tarjetas.jpg",
-                precio: 2000,
-                categoria: "Souvenir"
-            },
-            {
-                nombre: "Tarjetas",
-                rutaImagen: "./Recursos/tarjetas.jpg",
-                precio: 2000,
-                categoria: "Souvenir"
-            }
-        ]
-    ]
-}
-let tiendaHTML = ''
 
-
-tienda.categorias.forEach((categoria)=>{
-    tiendaHTML += `<section id="${categoria[0].categoria}">
-    <h3>${categoria[0].categoria}</h3>
-    <ul class="catalogo-lst" id="catalogo${categoria[0].categoria}">`
-    categoria.forEach((producto)=>{
-        //El Parametro producto contendra en cada interaccion un producto differente
-        tiendaHTML += `
-        <li>
-        <img class="foto-catalogo" src=${producto.rutaImagen} alt="Latas" width="336" height="252">
-        <ul>
-            <li>Nombre: ${producto.nombre}</li>
-            <li>Precio: $${producto.precio}</li>
-            <li>Descripcion:</li>
-        </ul>
-        </li>
-        `
+let inventario = {categorias: [[{
+    nombre: "Latita",
+    rutaImagen: "./Recursos/latas.jpg",
+    precio: 2000,
+    categoria: "Cumpleaños"
+}],[],[]]}
+fetch("inventario.json")
+    .then((res) => {
+        if (!res.ok) {
+            throw {ok: false, msg: "error 404"};
+        }
+        return res.json()
     })
-    tiendaHTML += `
-    </ul>
-    </section>`
-})
-Tienda.innerHTML = tiendaHTML
-
-//Modulo no implementado problema con MIME
-/* cumpleaños.innerHTML = make_innerHTML(productoHTML, tienda) */
+    .then((data) => {
+        inventario.categorias[0] = data[0]
+        inventario.categorias[1] = data[1]
+        inventario.categorias[2] = data[2]
+        Tienda.innerHTML = make_innerHTML(inventario)
+    })
+    .catch((err) => console.log(err))
--- practica-prof-html/funciones.js
+++ practica-prof-html/funciones.js
@@ -1,18 +1,27 @@
-function make_innerHTML(HTML, lista){
-    lista.productos.forEach((producto)=>{
+export function make_innerHTML(lista){
+    let HTML = ''
+    lista.categorias.forEach((categoria)=>{
         //El Parametro producto contendra en cada interaccion un producto differente
         HTML += `
-        <li>
-        <img class="foto-catalogo" src=${producto.rutaImagen} alt="Latas" width="336" height="252">
-        <ul>
-            <li>Nombre: ${producto.nombre}</li>
-            <li>Precio: $${producto.precio}</li>
-            <li>Descripcion:</li>
-        </ul>
-        </li>
-        `
+        <section id="${categoria[0].categoria}">
+        <h3>${categoria[0].categoria}</h3>
+        <ul class="catalogo-lst" id="catalogo${categoria[0].categoria}">`
+            categoria.forEach((producto)=>{
+            //El Parametro producto contendra en cada interaccion un producto differente
+                HTML += `
+                    <li>
+                    <img class="foto-catalogo" src=${producto.rutaImagen} alt="Latas" width="336" height="252">
+                    <ul>
+                        <li>Nombre: ${producto.nombre}</li>
+                        <li>Precio: $${producto.precio}</li>
+                        <li>Descripcion:</li>
+                    </ul>
+                    </li>
+                    `
+    })
+    HTML += `
+    </ul>
+    </section>`
     })
     return HTML
 }
-
-export default function make_innerHTML(){}
